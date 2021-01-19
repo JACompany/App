b4dc0a79c1968c97ee75358373da71e62c77230a
daniel-wang-design <daniel.wang04@hotmail.com>
2021-01-18 21:31:32 -0500

    update

--- lib/globalValues.dart
+++ lib/globalValues.dart
@@ -2,7 +2,10 @@ library values.globals;
 
 import 'package:app/storage.dart';
 
+//productive hours
 const int total_hours = 0;
+const int user_goal = 0;
+const int user_hours_day = 0;
 
 // storing user tasks
 final Storage tasks_storage = Storage("tasks");
