3007fc404c11aa4a294e4846d4937777ae2431cf
Mr-Royyy <jimmy.roy2006@gmail.com>
2021-01-20 17:10:54 -0500

    added in the axis hopefully (daniel checking)

--- lib/jimmy.dart
+++ lib/jimmy.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'globalValues.dart' as values;
 import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,6 +26,53 @@ class MyApp extends StatelessWidget {
 class ProgressGraph extends StatefulWidget {
   @override
   _ProgressGraph createState() => _ProgressGraph();
+}
+class LineTitles {
+  static getTitleData() => FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 35,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff68737d),
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 2:
+                return 'MAR';
+              case 5:
+                return 'JUN';
+              case 8:
+                return 'SEP';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (value) => const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '10k';
+              case 3:
+                return '30k';
+              case 5:
+                return '50k';
+            }
+            return '';
+          },
+          reservedSize: 35,
+          margin: 12,
+        ),
+      );
 }
 
 class _ProgressGraph extends State<ProgressGraph> {
@@ -50,5 +98,4 @@ class _ProgressGraph extends State<ProgressGraph> {
         ),
       ),
     );
-  }
 }
