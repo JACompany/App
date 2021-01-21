463751841cfcbd1c38176280008c186cf51b68e7
Mr-Royyy <jimmy.roy2006@gmail.com>
2021-01-20 22:20:39 -0500

    .

--- lib/jimmy.dart
+++ lib/jimmy.dart
@@ -39,22 +39,7 @@ class _ProgressGraph extends State<ProgressGraph> {
       child: Container(
         width: MediaQuery.of(context).size.width / 2,
         height: MediaQuery.of(context).size.height / 2,
-<<<<<<< HEAD
-        child: Sparkline(
-          data: values.past_hours,
-          lineWidth: 5.0,
-          lineColor: Colors.blue,
-          pointsMode: PointsMode.all,
-          pointSize: 12.0,
-          pointColor: Colors.amber,
-          fillMode: FillMode.below,
-          fillGradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.grey[300], Colors.grey],
-=======
-        child: LineChart(
-            LineChartData(backgroundColor: Colors.white, lineBarsData: [
+        child: LineChart(LineChartData(lineBarsData: [
           LineChartBarData(
             colors: [Colors.blue],
             spots: [
@@ -66,61 +51,17 @@ class _ProgressGraph extends State<ProgressGraph> {
               FlSpot(6, 3),
               FlSpot(7, 4),
             ],
->>>>>>> 3e01d29f9b77b40b90c7c122dc53e265b7268107
           ),
-        ),
+        ])),
+        // child: Sparkline(
+        //   data: values.past_hours,
+        //   lineWidth: 8.0,
+        //   lineColor: Colors.blue,
+        //   pointsMode: PointsMode.all,
+        //   pointSize: 16.0,
+        //   pointColor: Colors.grey,
+        // ),
       ),
     );
   }
-<<<<<<< HEAD
-}
-
-class LineTitles {
-  static getTitleData() => FlTitlesData(
-        show: true,
-        bottomTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 35,
-          getTextStyles: (value) => const TextStyle(
-            color: Color(0xff68737d),
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 2:
-                return 'WEEK 1';
-              case 5:
-                return 'WEEK 2';
-              case 8:
-                return 'WEEK 3';
-            }
-            return '';
-          },
-          margin: 8,
-        ),
-        leftTitles: SideTitles(
-          showTitles: true,
-          getTextStyles: (value) => const TextStyle(
-            color: Color(0xff67727d),
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-          ),
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 1:
-                return '10k';
-              case 3:
-                return '30k';
-              case 5:
-                return '50k';
-            }
-            return '';
-          },
-          reservedSize: 35,
-          margin: 12,
-        ),
-      );
-=======
->>>>>>> 3e01d29f9b77b40b90c7c122dc53e265b7268107
 }
