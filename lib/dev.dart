9732cfa3360c6a83e98a9feb1f13f43b7218c8e5
daniel-wang-design <daniel.wang04@hotmail.com>
2021-01-21 19:25:29 -0500

    update

--- lib/dev.dart
+++ lib/dev.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'globalValues.dart' as values;
+import 'dart:async';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,35 +11,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jimmy Test File',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        fontFamily: "Arial",
-        brightness: Brightness.light,
-      ),
-      home: ProgressGraph(),
+      home: LinearProgressIndicatorApp(),
     );
   }
 }
 
-class ProgressGraph extends StatefulWidget {
+class LinearProgressIndicatorApp extends StatefulWidget {
   @override
-  _ProgressGraph createState() => _ProgressGraph();
+  State<StatefulWidget> createState() {
+    return LinearProgressIndicatorAppState();
+  }
 }
 
-class _ProgressGraph extends State<ProgressGraph> {
+class LinearProgressIndicatorAppState
+    extends State<LinearProgressIndicatorApp> {
+  bool _loading;
+  double _progressValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = false;
+    _progressValue = 0.0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Jimmy Test",
-          style: TextStyle(color: Colors.blue),
-        ),
-        iconTheme: IconThemeData(color: Colors.blue),
+        title: Text("Flutter Linear Progress Bar"),
       ),
-      body: Text("Test"),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(12.0),
+          child: _loading
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    LinearProgressIndicator(
+                      backgroundColor: Colors.cyanAccent,
+                      valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
+                      value: _progressValue,
+                    ),
+                    Text('${(_progressValue * 100).round()}%'),
+                  ],
+                )
+              : Text('Press button for downloading',
+                  style: TextStyle(fontSize: 25)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _loading = !_loading;
+            _updateProgress();
+          });
+        },
+        tooltip: 'Download',
+        child: Icon(Icons.cloud_download),
+      ),
     );
+  }
+
+  // this function updates the progress value
+  void _updateProgress() {
+    const oneSec = const Duration(seconds: 1);
+    new Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.1;
+        // we "finish" downloading here
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          return;
+        }
+      });
+    });
   }
 }
