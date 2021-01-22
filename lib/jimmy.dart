6363993fc9216708d1eea0845764b55ee706516d
daniel-wang-design <daniel.wang04@hotmail.com>
2021-01-21 19:37:00 -0500

    update

--- lib/jimmy.dart
+++ lib/jimmy.dart
@@ -34,88 +34,25 @@ class _ProgressGraph extends State<ProgressGraph> {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        color: Colors.white,
-        width: MediaQuery.of(context).size.width / 2,
-        height: MediaQuery.of(context).size.height / 2,
-        child: LineChart(
-          LineChartData(lineBarsData: [
-            LineChartBarData(
-              colors: [Colors.blue],
-              spots: [
-                FlSpot(0, 3),
-                FlSpot(2, 2),
-                FlSpot(3, 5),
-                FlSpot(4, 3.1),
-                FlSpot(5, 4),
-                FlSpot(6, 3),
-                FlSpot(7, 4),
-              ],
-            ),
-
-
-            LineChartData mainData () {
-    return LineChartData(
-      gridData: FlGridData(
-        show: true,
-        drawVerticalLine: true,
-        getDrawingHorizontalLine: (value) {
-          return FlLine(
-            color: const Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-        getDrawingVerticalLine: (value) {
-          return FlLine(
-            color: const Color(0xff37434d),
-            strokeWidth: 1,
-          );
-        },
-      ),
-      titlesData: FlTitlesData(
-        show: true,
-        bottomTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 22,
-          getTextStyles: (value) =>
-              const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 2:
-                return '1';
-              case 5:
-                return '2';
-              case 8:
-                return '3';
-            }
-            return '';
-          },
-          margin: 8,
-        ),
-        leftTitles: SideTitles(
-          showTitles: true,
-          getTextStyles: (value) => const TextStyle(
-            color: Color(0xff67727d),
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
-          ),
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 1:
-                return '1k';
-              case 3:
-                return '3k';
-              case 5:
-                return '5k';
-            }
-            return '';
-          },
-          reservedSize: 28,
-          margin: 12,
-        ),
-      ));
-            }]),
-        ),
-      ),
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width / 2,
+          height: MediaQuery.of(context).size.height / 2,
+          child: LineChart(
+            LineChartData(lineBarsData: [
+              LineChartBarData(
+                colors: [Colors.blue],
+                spots: [
+                  FlSpot(0, 3),
+                  FlSpot(2, 2),
+                  FlSpot(3, 5),
+                  FlSpot(4, 3.1),
+                  FlSpot(5, 4),
+                  FlSpot(6, 3),
+                  FlSpot(7, 4),
+                ],
+              ),
+            ]),
+          )),
     );
   }
 }
