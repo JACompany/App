ce807b136e6ac9a1330f248db8bd7f7fd10d7d66
daniel-wang-design <daniel.wang04@hotmail.com>
2021-01-28 18:50:10 -0500

    update

--- lib/globalValues.dart
+++ lib/globalValues.dart
@@ -4,8 +4,8 @@ import 'package:app/storage.dart';
 import 'package:flutter/cupertino.dart';
 
 //productive hours
-const double total_hours = 0;
-const double user_goal = 0;
+const double total_hours = 15;
+const double user_goal = 20;
 const double user_hours_day = 0;
 const List<double> past_hours = [2.0, 3.0, 5.4, 1.1, 7.4, 4.5, 2.2];
 
