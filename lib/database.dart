5ee6375bbe696b9fdf1435cbb67a697984a6ba97
daniel-wang-design <daniel.wang04@hotmail.com>
2021-02-04 08:27:52 -0500

    update

--- lib/database.dart
+++ lib/database.dart
@@ -1,20 +1,52 @@
 //https://www.youtube.com/watch?v=DqJ_KjFzL9I
-import 'package:flutter/material.dart';
 
-void main() => runApp(database());
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(database());
+}
 
 class database extends StatelessWidget {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Database"),
         ),
-        body: ListView(
-          children: [Text("Person 1")],
-        ),
+        body: tiles(context),
       ),
+    );
+  }
+
+  List<Widget> list = [];
+  read() async {
+    await firestore
+        .collection("leaderboard")
+        .get()
+        .then((value) => value.docs.forEach((element) {
+              list.add(ListTile(
+                title: Text(element.get('name')),
+                trailing: Text(element.get('score').toString()),
+              ));
+            }));
+  }
+
+  ListView tiles(context) {
+    // firestore.collection("leaderboard").snapshots().listen((result) {
+    //   result.docs.forEach((element) {
+    //     list.add(element.data());
+    //   });
+    // });
+    read();
+    return ListView(
+      children: list,
     );
   }
 }
