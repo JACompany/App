7a181a4afd384aeb20466822adc41e0566339963
daniel-wang-design <daniel.wang04@hotmail.com>
2021-02-06 17:35:45 -0500

    update

--- lib/jimmy.dart
+++ lib/jimmy.dart
@@ -3,6 +3,10 @@ import 'package:flutter_sparkline/flutter_sparkline.dart';
 import 'globalValues.dart' as values;
 import 'package:sizer/sizer.dart';
 
+import 'homepage.dart';
+import 'leaderboard.dart';
+import 'progress_page.dart';
+
 void main() => runApp(LineChartSample1());
 
 class LineChartSample1 extends StatefulWidget {
@@ -19,50 +23,115 @@ class LineChartSample1State extends State<LineChartSample1> {
         return OrientationBuilder(builder: (context, orientation) {
           SizerUtil().init(constraints, orientation);
           return MaterialApp(
-              home: Scaffold(
-                  appBar: AppBar(),
-                  body: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 10,
-                                  height: 100,
-                                  color: Colors.black,
-                                ),
-                                SizedBox(width: 1.0.w),
-                                Sparkline(
-                                  data: values.past_hours,
-                                  pointsMode: PointsMode.all,
-                                  pointSize: 12.0,
-                                  pointColor: Colors.blue,
-                                ),
-                              ],
-                            ),
-                            Column(
-                              verticalDirection: VerticalDirection.up,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  "Days of the Week",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                Container(
-                                  width: 350,
-                                  height: 10,
-                                  color: Colors.black,
-                                )
-                              ],
-                            ),
-                          ]))));
+            home: Profile(),
+          );
         });
       },
     );
+  }
+}
+
+class Profile extends StatefulWidget {
+  @override
+  _Profile createState() => _Profile();
+}
+
+class _Profile extends State<Profile> {
+  bool get maintainState {
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: values.color_green,
+          title: Text(
+            "Profile",
+            style: TextStyle(
+              fontSize: 4.0.h,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: null,
+            icon: Icon(Icons.account_circle),
+            disabledColor: Colors.black,
+            iconSize: 3.0.h,
+          )),
+      body: Text("Profile"),
+      bottomNavigationBar: Container(
+        height: 8.0.h,
+        alignment: Alignment.bottomCenter,
+        color: values.color_green,
+        child: Row(
+          children: [
+            Expanded(
+                child: IconButton(
+              icon: Icon(
+                Icons.timer,
+                color:
+                    values.current_page == "home" ? Colors.white : Colors.black,
+              ),
+              onPressed: onPressed1,
+              iconSize: 6.0.h,
+            )),
+            Expanded(
+                child: IconButton(
+              icon: Icon(Icons.insights),
+              onPressed: onPressed2,
+              iconSize: 6.0.h,
+            )),
+            Expanded(
+                child: IconButton(
+              icon: Icon(Icons.leaderboard),
+              onPressed: onPressed3,
+              iconSize: 6.0.h,
+            )),
+            Expanded(
+                child: IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: onPressed4,
+              iconSize: 6.0.h,
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onPressed1() {
+    values.current_page = "home";
+    Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+            pageBuilder: (context, animation, animation2) => Home(),
+            transitionDuration: Duration(seconds: 0)),
+        (route) => false);
+  }
+
+  void onPressed2() {
+    values.current_page = "progress";
+    Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+            pageBuilder: (context, animation, animation2) => ProgressPage(),
+            transitionDuration: Duration(seconds: 0)),
+        (route) => false);
+  }
+
+  void onPressed3() {
+    values.current_page = "leaderboard";
+    Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+            pageBuilder: (context, animation, animation2) => Leaderboard(),
+            transitionDuration: Duration(seconds: 0)),
+        (route) => false);
+  }
+
+  void onPressed4() {
+    values.current_page = "profile";
+    Navigator.of(context).pushAndRemoveUntil(
+        PageRouteBuilder(
+            pageBuilder: (context, animation, animation2) => Profile(),
+            transitionDuration: Duration(seconds: 0)),
+        (route) => false);
   }
 }
