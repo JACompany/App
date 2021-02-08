46aac6668e0f27e14aa1ff446e5fa3cd158b55a9
daniel-wang-design <daniel.wang04@hotmail.com>
2021-02-07 19:07:21 -0500

    update

--- lib/jimmy.dart
+++ lib/jimmy.dart
@@ -41,6 +41,7 @@ class _Profile extends State<Profile> {
     return false;
   }
 
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +59,7 @@ class _Profile extends State<Profile> {
             disabledColor: Colors.black,
             iconSize: 3.0.h,
           )),
-      body: Text("Profile"),
+      body: update_user_details(),
       bottomNavigationBar: Container(
         height: 8.0.h,
         alignment: Alignment.bottomCenter,
@@ -99,42 +100,38 @@ class _Profile extends State<Profile> {
     );
   }
 
-  void TextFormField() {
-    @override
-    Widget build(BuildContext context) {
-      // Build a Form widget using the _formKey created above.
-      return Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              //The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
+  Widget update_user_details() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Prcessing data..."),
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  );
                 }
-                return null;
               },
+              child: Text('Submit'),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false
-                  // otherwise.
-                  if (_formKey.currentState.validate()) {
-                    // If the form is valid, display a Snackbar.
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Processing Data')));
-                  }
-                },
-                child: Text('Submit'),
-              ),
-            ),
-          ],
-        ),
-      );
-    }
+          ),
+        ],
+      ),
+    );
   }
 
   void onPressed1() {
