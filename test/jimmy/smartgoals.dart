d5f9637a0ef68bf1ea66b560d8994e2a56b54da4
Mr-Royyy <jimmy.roy2006@gmail.com>
2021-03-04 19:14:25 -0500

    dad

--- test/jimmy/smartgoals.dart
+++ test/jimmy/smartgoals.dart
@@ -48,22 +48,67 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Specific',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Text(
             'What do you want to acheive?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
           ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
+          ),
           Text(
             'When do you want your goal to be finished?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
           ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
+          ),
           Text(
             'Who needs to be involved to accomplish this goal?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
           ),
           Text(
             'Why should you achieve this goal exactly?',
@@ -89,7 +134,7 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Measurable',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Text(
             'How can you measure progress and know if you have successfully met your goal?',
@@ -115,17 +160,47 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Achievable',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Text(
             'Are you capable of achieving the goal?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
           ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
+          ),
           Text(
             'Do you have the needed skills?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
           ),
           Text(
             'If you do not have the skills, how can you build them?',
@@ -151,12 +226,27 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Relevent',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Text(
             'Why should you achieve this goal?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
           ),
           Text(
             'What is the impact?',
@@ -182,12 +272,27 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Timely',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           Text(
             'What is the due date of your goal?',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: false,
+            decoration: InputDecoration(
+                hintText: 'Enter text here',
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 32.0),
+                    borderRadius: BorderRadius.circular(5.0)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    borderRadius: BorderRadius.circular(5.0))),
+            onChanged: (value) {
+              //Do something with this value
+            },
           ),
           Text(
             'Can the goal be achieved until this date?',
@@ -213,7 +318,7 @@ class MyCustomFormState extends State<MyCustomForm> {
             'Smart Goal',
             textAlign: TextAlign.left,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
           TextFormField(
             validator: (value) {
